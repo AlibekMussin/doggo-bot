7419a8923e9dadeeb8d6c632494a3113c9d7673c
Алибек <svarog221989@gmail.com>
2023-05-16 17:49:57 +0600

    dbg

--- index.js
+++ index.js
@@ -36,9 +36,7 @@ bot.on('message', async (msg) => {
     });    
   }
 
-  console.log('1111');
-    console.log(msg);
-
+  
     if (msg?.web_app_data?.data)
     {
       try {
@@ -58,32 +56,23 @@ bot.on('message', async (msg) => {
     }
 });
 
-app.post('/web-data', async (req, res) =>  {
-  const {queryId, products, totalPrice} = req.body;
+app.post('/web-data', async (req, res) => {
+  const {queryId, products = [], totalPrice} = req.body;
   try {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Успешная покупка',
-      input_message_content:{
-        message_text:"Заказ на сумму "+ totalPrice + "тнг успешно создан. Ожидайте звонка менеджера"
-      }
-    });
-    return res.status(200).json({});
-    
-  } catch (error) {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Не удалось сделать заказ',
-      input_message_content:{
-        message_text:error
-      }
-    })
-    return res.status(500).json({});
+      await bot.answerWebAppQuery(queryId, {
+          type: 'article',
+          id: queryId,
+          title: 'Успешная покупка',
+          input_message_content: {
+              message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+          }
+      })
+      return res.status(200).json({});
+  } catch (e) {
+      return res.status(500).json({})
   }
-  
-});
+})
 
-const PORT = 8009; 
-app.listen(PORT, ()=>console.log('server started, bitcheeeees. PORT: ' + PORT));
+const PORT = 8009;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
