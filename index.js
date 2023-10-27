60da5a69198bc2e963767eb335f4a5f9f1160501
Алибек <svarog221989@gmail.com>
2023-10-27 14:41:25 +0600

    delete not need code

--- index.js
+++ index.js
@@ -23,18 +23,9 @@ app.use(cors());
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
-
   if (text == '/start')
   {
-    await bot.sendMessage(chatId, 'Нажмите на кнопку Каталог товаров и выберите товары, которые вам нужны', 
-    // {
-    //   reply_markup:{
-    //     inline_keyboard: [
-    //       [{text: "Каталог товаров", web_app:{url: webAppUrl }}]
-    //     ]
-    //   }
-    // }
-    );    
+    await bot.sendMessage(chatId, 'Нажмите на кнопку Каталог товаров и выберите товары, которые вам нужны', );    
   }
 });
 
